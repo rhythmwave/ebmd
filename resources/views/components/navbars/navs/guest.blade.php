a7e3c139791ba0b146d72d144b6daa796945b4dc
bayu <bayu@neuronworks.co.id>
2023-07-06 10:35:25 +0700

    Develop using laravel 10

---
+++ resources/views/components/navbars/navs/guest.blade.php
@@ -0,0 +1,11 @@
+@props(['signin', 'signup'])
+
+<!-- <nav
+    class="navbar navbar-expand-lg blur border-radius-lg top-0 z-index-3 shadow position-absolute mt-4 py-2 start-0 end-0 mx-4">
+    <div class="container-fluid ps-2 pe-0">
+        
+        <div class="collapse navbar-collapse" id="navigation">
+            
+        </div>
+    </div>
+</nav> -->
---
+++ resources/views/order/create.blade.php
@@ -0,0 +1,146 @@
+<script src="{{ asset('assets') }}/js/dropzone-min.js"></script>
+<link href="{{ asset('assets') }}/css/dropzone.css" rel="stylesheet" type="text/css" />
+<x-layout bodyClass="g-sidenav-show bg-gray-200">
+    <x-navbars.sidebar activePage="tables"></x-navbars.sidebar>
+    <div class="main-content position-relative bg-gray-100 max-height-vh-100 h-100">
+        <!-- Navbar -->
+        <x-navbars.navs.auth titlePage='Permohonan Baru'></x-navbars.navs.auth>
+        <!-- End Navbar -->
+        <div class="container-fluid px-2 px-md-4">
+            <div class="page-header min-height-100 border-radius-xl mt-4">
+                <!-- <span class="mask  bg-gradient-primary  opacity-6"></span> -->
+            </div>
+            <div class="card card-body mx-3 mx-md-4 mt-n6">
+                <div class="row gx-4 mb-2">
+                    <div class="col-auto">
+                        <h3>Form Usulan Permohonan Penghapusan BMD</h3>
+                    </div>
+                    <div class="col-lg-4 col-md-6 my-sm-auto ms-sm-auto me-sm-0 mx-auto mt-3">
+                        <div class="nav-wrapper position-relative end-0">
+                            <ul class="nav nav-pills nav-fill p-1" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link mb-0 px-0 py-1 active " data-bs-toggle="tab" href="javascript:;"
+                                        role="tab" aria-selected="true">
+                                        <i class="material-icons text-lg position-relative">home</i>
+                                        <span class="ms-1">App</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mb-0 px-0 py-1 " data-bs-toggle="tab" href="javascript:;"
+                                        role="tab" aria-selected="false">
+                                        <i class="material-icons text-lg position-relative">email</i>
+                                        <span class="ms-1">Messages</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mb-0 px-0 py-1 " data-bs-toggle="tab" href="javascript:;"
+                                        role="tab" aria-selected="false">
+                                        <i class="material-icons text-lg position-relative">settings</i>
+                                        <span class="ms-1">Settings</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="row gx-4 mb-2">
+                    <div class="col-md-4 align-items-center">
+                        <label class="form-label">Nomor Surat</label>
+                        <input type="text" name="no-surat" class="form-control border border-2 p-2" value=''>
+                    </div>
+                    <div class="col-md-4 align-items-center">
+                        <label class="form-label">Jenis</label>
+                        <input type="text" name="jenis" class="form-control border border-2 p-2" value=''>
+                    </div>
+                    <div class="col-md-4 align-items-center">
+                        <label class="form-label">Tanggal</label>
+                        <input type="text" name="tanggal" class="form-control border border-2 p-2" value=''>
+                    </div>
+                </div>
+                <div class="card card-plain h-100">
+                    <div class="card-header pb-0 p-3">
+                        <div class="row">
+                            <div class="col-md-8 d-flex align-items-center">
+                                <h6 class="mb-3">Upload Dokumen</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body p-3">
+                        <!-- @if (session('status'))
+                        <div class="row">
+                            <div class="alert alert-success alert-dismissible text-white" role="alert">
+                                <span class="text-sm">{{ Session::get('status') }}</span>
+                                <button type="button" class="btn-close text-lg py-3 opacity-10"
+                                    data-bs-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        </div>
+                        @endif
+                        @if (Session::has('demo'))
+                                <div class="row">
+                                    <div class="alert alert-danger alert-dismissible text-white" role="alert">
+                                        <span class="text-sm">{{ Session::get('demo') }}</span>
+                                        <button type="button" class="btn-close text-lg py-3 opacity-10"
+                                            data-bs-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                </div>
+                        @endif -->
+                        <form method='POST' action='{{ route('verify') }}'>
+                            @csrf
+                            <div class="row">
+                                
+                                <div class="mb-3 col-md-6">
+                                    <label> Permohonan Pemindahtanganan BMD </label>
+                                    <button type="button" class="btn btn-secondary btn-sm">Download Format</button>
+                                </div>
+                                
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Name</label>
+                                    <input type="text" name="name" class="form-control border border-2 p-2" value='{{ old('name', auth()->user()->name) }}'>
+                                    @error('name')
+                                <p class='text-danger inputerror'>{{ $message }} </p>
+                                @enderror
+                                </div>
+                               
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Phone</label>
+                                    <input type="number" name="phone" class="form-control border border-2 p-2" value='{{ old('phone', auth()->user()->phone) }}'>
+                                    @error('phone')
+                                    <p class='text-danger inputerror'>{{ $message }} </p>
+                                    @enderror
+                                </div>
+                                
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Location</label>
+                                    <input type="text" name="location" class="form-control border border-2 p-2" value='{{ old('location', auth()->user()->location) }}'>
+                                    @error('location')
+                                    <p class='text-danger inputerror'>{{ $message }} </p>
+                                    @enderror
+                                </div>
+                                
+                                <div class="mb-3 col-md-12">
+                                    <label for="floatingTextarea2">About</label>
+                                    <textarea class="form-control border border-2 p-2"
+                                        placeholder=" Say something about yourself" id="floatingTextarea2" name="about"
+                                        rows="4" cols="50">{{ old('about', auth()->user()->about) }}</textarea>
+                                        @error('about')
+                                        <p class='text-danger inputerror'>{{ $message }} </p>
+                                        @enderror
+                                </div>
+                            </div>
+                            <button type="submit" class="btn bg-gradient-dark">Submit</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <x-footers.auth></x-footers.auth>
+    </div>
+    <x-plugins></x-plugins>
+
+</x-layout>
